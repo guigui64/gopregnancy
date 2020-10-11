96f1fd87ff95a2d9efe224eeaf2529d58c507791
guigui64 <guillaume.comte10@gmail.com>
2020-10-11 12:55:59 +0200

    good/wrong/animated/queries

--- main.go
+++ main.go
@@ -16,13 +16,14 @@ import (
 )
 
 type Page struct {
-	User    string
-	Message string
-	Tip     string
-	Offset  int
+	User           string
+	CodedMessage   string
+	DecodedMessage string
+	Tip            string
+	Offset         int
 }
 
-var templates = template.Must(template.ParseFiles("index.html"))
+var templates = template.Must(template.ParseFiles("index.html", "good.html", "wrong.html"))
 
 func loadPage(name string) (*Page, error) {
 	filename := name + ".txt"
@@ -32,7 +33,7 @@ func loadPage(name string) (*Page, error) {
 	}
 	lines := strings.Split(string(content), "\n")
 	offset, _ := strconv.Atoi(lines[4])
-	return &Page{User: lines[0], Message: lines[2], Tip: lines[3], Offset: offset}, nil
+	return &Page{User: lines[0], DecodedMessage: lines[1], CodedMessage: lines[2], Tip: lines[3], Offset: offset}, nil
 }
 
 func renderTemplate(w http.ResponseWriter, p *Page) {
@@ -43,11 +44,18 @@ func renderTemplate(w http.ResponseWriter, p *Page) {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	// TODO queries
-	name := "z4z401b7e977"
-	p, err := loadPage(name)
+	q := r.URL.Query()
+	name, ok := q["user"]
+	if !ok {
+		// TODO 404 page
+		http.Error(w, "No user specified", http.StatusNotFound)
+		return
+	}
+	p, err := loadPage(name[0])
 	if err != nil {
+		// TODO 404 page
 		http.Error(w, err.Error(), http.StatusNotFound)
+		return
 	}
 	renderTemplate(w, p)
 }
