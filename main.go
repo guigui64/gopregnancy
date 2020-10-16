3de005f13ab824cd9e7ae8a266e1ee20987b42b2
Guillaume Comte <guillaume.comte@airbus.com>
2020-10-16 15:11:11 +0200

    more

--- main.go
+++ main.go
@@ -15,51 +15,34 @@ import (
 	"strings"
 )
 
+// parsed passwords
+var passwords []string
+
+// html templates
+var templates = template.Must(template.ParseFiles("index.html", "good0.html", "good1.html", "wrong.html", "404.html", "end.html"))
+
+// template struct
 type Page struct {
 	User           string
 	CodedMessage   string
 	DecodedMessage string
 	Tip            string
-	Offset         int
+	Offset         byte
+	Guess          string
+	Code           string
+	Steps          []string
+	CurrentStep    int
 }
 
-var templates = template.Must(template.ParseFiles("index.html", "good.html", "wrong.html"))
-
-func loadPage(name string) (*Page, error) {
-	filename := name + ".txt"
-	content, err := ioutil.ReadFile(filename)
+func parsePasswords() {
+	content, err := ioutil.ReadFile("passwords.txt")
 	if err != nil {
-		return nil, err
+		log.Panic(err)
 	}
-	lines := strings.Split(string(content), "\n")
-	offset, _ := strconv.Atoi(lines[4])
-	return &Page{User: lines[0], DecodedMessage: lines[1], CodedMessage: lines[2], Tip: lines[3], Offset: offset}, nil
+	passwords = strings.Split(string(content), "\n")
 }
 
-func renderTemplate(w http.ResponseWriter, p *Page) {
-	err := templates.ExecuteTemplate(w, "index.html", p)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	q := r.URL.Query()
-	name, ok := q["user"]
-	if !ok {
-		// TODO 404 page
-		http.Error(w, "No user specified", http.StatusNotFound)
-		return
-	}
-	p, err := loadPage(name[0])
-	if err != nil {
-		// TODO 404 page
-		http.Error(w, err.Error(), http.StatusNotFound)
-		return
-	}
-	renderTemplate(w, p)
-}
-
+// Shift letters
 func shift(s string, offset byte) string {
 	b := []byte(s)
 	for i, bi := range b {
@@ -71,7 +54,84 @@ func shift(s string, offset byte) string {
 	return string(b)
 }
 
+func loadPage(name string) (*Page, error) {
+	filename := name + ".txt"
+	content, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	lines := strings.Split(string(content), "\n")
+	offset, _ := strconv.Atoi(lines[4])
+	return &Page{User: lines[0], DecodedMessage: lines[1], CodedMessage: lines[2], Tip: lines[3], Offset: byte(offset), Code: lines[5], Steps: lines[6:]}, nil
+}
+
+func renderTemplate(w http.ResponseWriter, p *Page, src string) {
+	err := templates.ExecuteTemplate(w, src+".html", p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func notFound(w http.ResponseWriter, reason string) {
+	err := templates.ExecuteTemplate(w, "404.html", reason)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	q := r.URL.Query()
+	name, ok := q["user"]
+	if !ok {
+		notFound(w, "No user specified")
+		return
+	}
+	p, err := loadPage(name[0])
+	if err != nil {
+		notFound(w, err.Error())
+		return
+	}
+	src := "index"
+	step := q.Get("step")
+	guess := q.Get("guess")
+	p.Guess = guess
+	switch step {
+	case p.Steps[0]:
+		guessN, _ := strconv.Atoi(guess)
+		if guessN == int(p.Offset) {
+			p.CurrentStep = 1
+			src = "good0"
+		} else {
+			p.CurrentStep = 0
+			p.Guess = shift(p.CodedMessage, byte(guessN))
+			src = "wrong"
+		}
+	case p.Steps[1]:
+		if guess == passwords[0] {
+			p.CurrentStep = 2
+			src = "good1"
+		} else {
+			p.CurrentStep = 1
+			src = "wrong"
+		}
+	case p.Steps[2]:
+		if guess == passwords[1] {
+			p.CurrentStep = 2
+			src = "end"
+		} else {
+			p.CurrentStep = 2
+			src = "wrong"
+		}
+	}
+	log.Printf("[%s] step=%d guess=%s res=%s", name, p.CurrentStep, guess, src)
+	renderTemplate(w, p, src)
+}
+
 func main() {
+	log.Println("Parsing passwords file...")
+	parsePasswords()
+
+	log.Println("Starting server...")
 	http.HandleFunc("/", handler)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	log.Println("Listening on http://localhost:8080/")
